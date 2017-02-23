57b0215a71a912626a97fa3f17ec24855c21d4e7
Alexander Mahdybor <thezaksid@gmail.com>
2017-02-23 12:04:15 +0200

    Added 'check' and 'delete' actions

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+const todos = [
+    { title: 'Learn JS', completed: true },
+    { title: 'Learn Angular 2', completed: false },
+    { title: 'Write app', completed: false }
+];
+
 @Component({
     moduleId: module.id,
     selector: 'app',
@@ -8,4 +14,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'Angular 2Do';
+    todos = todos;
+
+    toggle(todo: any) {console.log('toggle', todo);
+        todo.completed = !todo.completed;
+    }
+
+    delete(todo: any) {
+        let index = this.todos.indexOf(todo);
+        if (index > -1) {
+            this.todos.splice(index, 1);
+        }
+    }
 }
